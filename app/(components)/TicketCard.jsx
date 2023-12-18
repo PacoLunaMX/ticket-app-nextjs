890e102e2de18065e106aecef07c6a1fe30c9588
PacoLunaMX <f.hinojosa9@hotmail.com>
2023-12-17 17:47:49 -0700

    first-commit

---
+++ app/(components)/TicketCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import DeleteBlock from './DeleteBlock'
+
+
+function TicketCard() {
+  return (
+    <div>
+        <DeleteBlock />
+        
+    </div>
+  )
+}
+
+export default TicketCard
---
+++ app/TicketPage/[id]/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import TicketCard from '@/app/(components)/TicketCard'
+
+function TicketPage({params}) {
+  
+  return (
+    <div>
+        <TicketCard></TicketCard>
+    </div>
+  )
+}
+
+export default TicketPage
